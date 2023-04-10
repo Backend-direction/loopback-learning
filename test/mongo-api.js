36914690e18a73ca4d3f38abf937485e9689a8aa
Volodymyr Pankiv <vpanki@softserveinc.com>
2023-04-10 14:54:27 +0300

    start test monogo

--- test/mongo-api.js
+++ test/mongo-api.js
@@ -1,12 +1,13 @@
 // Read .env file and set environment variables
 require('dotenv').config();
 const random = Math.floor(Math.random() * 100);
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 // Use official mongodb driver to connect to the server
 const { MongoClient, ObjectId } = require('mongodb');
 
 
-const url = '';
+const url = 'mongodb://d6b7bc78-0ee0-4-231-b9ee:LHMi6I1RyTotD4dGcx0NRJhX4f8IFlRYWv9iXu8agJuqvPlvJehKGpEgZHQbtmhCQqorGSWeNYn0ACDbIydCkg==@d6b7bc78-0ee0-4-231-b9ee.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@d6b7bc78-0ee0-4-231-b9ee@';
 const client = new MongoClient(url);
 
 async function main(){
